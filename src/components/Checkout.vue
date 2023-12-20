c3818a8cd1ccb08a326c7e0fc7d73a0129f00ac7
Dionis Barcari <dionisbarcari@Dioniss-MacBook-Pro.local>
2023-12-20 22:18:48 +0000

    project initiation

---
+++ src/components/Checkout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+
+<script>
+    export default {
+        name: "Checkout-Page",
+        params: {
+        
+        }
+    }
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>HELLOOOO World</div>
+</template>
+
+
+<script>
+    export default {
+        name: "Lessons-Page",
+        params: {
+        
+        }
+    }
+
+</script>
+
+
+<style scoped>
+
+</style>
