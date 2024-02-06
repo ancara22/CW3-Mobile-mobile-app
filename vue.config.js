3d73834dda631495d4a2b34fe2d5dca0c7ad7f06
Dionis Barcari <dionisbarcari@Dioniss-MacBook-Pro.local>
2024-02-06 18:54:53 +0000

    fix5

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './CW3-Mobile-mobile-app/',
+ // publicPath: './',
   pwa: {
     pwa: {
       workboxPluginMode: 'InjectManifest',
